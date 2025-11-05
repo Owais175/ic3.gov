902e13f247ae6c5174ca4c92cf74c38ba74f13de
Irtiza Ahmed <54776755+IrtizaAhmed131198@users.noreply.github.com>
2025-11-05 22:22:14 +0500

    add middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\SlowDownMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->append(SlowDownMiddleware::class);
         $middleware->alias([
             'role' => RoleMiddleware::class,
         ]);
